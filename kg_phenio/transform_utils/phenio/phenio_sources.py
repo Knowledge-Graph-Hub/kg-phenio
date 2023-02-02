4b42e7ef9ff44dcdfc9401f674f0b6f9a593ddb4
caufieldjh <j.harry.caufield@gmail.com>
2023-02-01 21:48:37 -0500

    Expanding infores maps; still missing some prefixes

--- kg_phenio/transform_utils/phenio/phenio_sources.py
+++ kg_phenio/transform_utils/phenio/phenio_sources.py
@@ -14,17 +14,73 @@ valid = True
 # with Biolink-compliant knowledge sources.
 
 # This maps CURIE prefixes to infores: names.
-infores_sources = {}
+# TODO: technically the names should be part of
+#       biolink:InformationResource objects
+infores_sources = {
+    "BFO": "bfo",
+    "biolink": "biolink",
+    "CHEBI": "chebi",
+    "CARO": "caro",
+    "CL": "cl",
+    "ECO": "eco",
+    "OBO": "obo", # Not quite right - these are extra OBO prefixes
+    "EMAPA": "emapa",
+    "ENVO": "envo",
+    "FAO": "fao",
+    "FBbt": "fbbt",
+    "FMA": "fma",
+    "faldo": "faldo",
+    "foaf": "foaf",
+    "FlyBase": "flybase",
+    "FOODON": "foodon",
+    "GO": "go",
+    "HP": "hp",
+    "IAO": "iao",
+    "MA": "ma",
+    "MF": "mf",
+    "MONDO": "mondo",
+    "MPATH": "mpath",
+    "MP": "mp",
+    "NBO": "nbo",
+    "NCBITaxon": "ncbitaxon",
+    "NCIT": "ncit",
+    "OBA": "oba",
+    "OBI": "obi",
+    "OGMS": "ogms",
+    "OIO": "oio",
+    "owl": "owl",
+    "PATO": "pato",
+    "PCO": "pco",
+    "PO": "po",
+    "PR": "pr",
+    "RO": "ro",
+    "RXCUI": "rxnorm", # TODO: check on these edges
+    "SO": "so",
+    "STATO": "stato",
+    "UBERON": "uberon",
+    "UMLS": "umls", # TODO: check on these edges
+    "UPHENO": "upheno",
+    "WBPhenotype": "wbphenotype",
+    "WBbt": "wbbt",
+    "ZFA": "zfa",
+    "ZP": "zp",
+}
+
+bad_prefixes = ["http", "DATA"]
+
+common_prefixes = ["BFO", "owl", "RO"]
 
 primary_knowledge_source = "infores:unknown"
 aggregator_knowledge_source = "infores:phenio"
 
 subj_curie_prefix = (str(row["subject"]).split(":"))[0]
+obj_curie_prefix = (str(row["object"]).split(":"))[0]
 
-if subj_curie_prefix in infores_sources:
+# TODO: make this more specific, as it won't always be true
+if subj_curie_prefix not in bad_prefixes:
     infores = infores_sources[subj_curie_prefix]
     primary_knowledge_source = f"infores:{infores}"
-
+    
 if row["category"] in ["biolink:category"]:
     valid = False
 
@@ -34,15 +90,15 @@ if row["category"] in ["biolink:category"]:
 #       as it isn't an association slot
 
 # Association
-association = Association(
-    id=row["id"],
-    subject=row["subject"],
-    predicate=row["predicate"],
-    object=row["object"],
-    #relation=row["relation"],
-    primary_knowledge_source=primary_knowledge_source,
-    aggregator_knowledge_source=aggregator_knowledge_source
-)
-
 if valid:
+    association = Association(
+        id=row["id"],
+        subject=row["subject"],
+        predicate=row["predicate"],
+        object=row["object"],
+        # relation=row["relation"],
+        primary_knowledge_source=primary_knowledge_source,
+        aggregator_knowledge_source=aggregator_knowledge_source,
+    )
+
     koza_app.write(association)
