dd7b02a9283913fce6e38fc6ab9b600d83be538b
caufieldjh <j.harry.caufield@gmail.com>
2023-02-03 18:53:35 -0500

    More edge infores updates

--- kg_phenio/transform_utils/phenio/phenio_sources.py
+++ kg_phenio/transform_utils/phenio/phenio_sources.py
@@ -17,53 +17,67 @@ valid = True
 # TODO: technically the names should be part of
 #       biolink:InformationResource objects
 infores_sources = {
+    "APO": "apo",  # TODO: check this one
     "BFO": "bfo",
-    "biolink": "biolink",
-    "CHEBI": "chebi",
+    "BSPO": "bspo",  # TODO: check this one
+    "BTO": "bto",  # TODO: check this one
     "CARO": "caro",
+    "CHEBI": "chebi",
+    "CHR": "chr",  # TODO: check this one
+    "CIO":  "cio",  # TODO: check this one
     "CL": "cl",
+    "CLO": "clo",  # TODO: check this one
+    "DDANAT": "ddanat",  # TODO: check this one
+    "DOID": "doid",  # TODO: check this one
     "ECO": "eco",
-    "OBO": "obo", # Not quite right - these are extra OBO prefixes
     "EMAPA": "emapa",
     "ENVO": "envo",
     "FAO": "fao",
     "FBbt": "fbbt",
+    "FBcv": "fbcv",
     "FMA": "fma",
-    "faldo": "faldo",
-    "foaf": "foaf",
-    "FlyBase": "flybase",
     "FOODON": "foodon",
+    "FlyBase": "flybase",
+    "FYPO": "fypo",
+    "GENO": "geno",
     "GO": "go",
     "HP": "hp",
     "IAO": "iao",
     "MA": "ma",
     "MF": "mf",
+    "MFOMD": "mfomd",  # TODO: check this
+    "MI": "mi",
     "MONDO": "mondo",
-    "MPATH": "mpath",
     "MP": "mp",
+    "MPATH": "mpath",
     "NBO": "nbo",
     "NCBITaxon": "ncbitaxon",
     "NCIT": "ncit",
     "OBA": "oba",
     "OBI": "obi",
+    "OBO": "obo",  # TODO: Not quite right - these are extra OBO prefixes
     "OGMS": "ogms",
     "OIO": "oio",
-    "owl": "owl",
+    "OMIM": "omim",  # TODO: certainly check these
     "PATO": "pato",
     "PCO": "pco",
     "PO": "po",
     "PR": "pr",
     "RO": "ro",
-    "RXCUI": "rxnorm", # TODO: check on these edges
+    "RXCUI": "rxnorm",  # TODO: check on these edges
     "SO": "so",
     "STATO": "stato",
     "UBERON": "uberon",
-    "UMLS": "umls", # TODO: check on these edges
+    "UMLS": "umls",  # TODO: check on these edges
     "UPHENO": "upheno",
     "WBPhenotype": "wbphenotype",
     "WBbt": "wbbt",
     "ZFA": "zfa",
     "ZP": "zp",
+    "biolink": "biolink",
+    "faldo": "faldo",
+    "foaf": "foaf",
+    "owl": "owl",
 }
 
 bad_prefixes = ["http", "DATA"]
@@ -80,9 +94,13 @@ obj_curie_prefix = (str(row["object"]).split(":"))[0]
 if subj_curie_prefix not in bad_prefixes:
     infores = infores_sources[subj_curie_prefix]
     primary_knowledge_source = f"infores:{infores}"
-    
-if row["category"] in ["biolink:category"]:
-    valid = False
+
+# Remove category relations
+# TODO: save these in case the category
+#       didn't propagate to the node
+for row_name in ["category", "predicate"]:
+    if row[row_name] == "biolink:category":
+        valid = False
 
 # TODO: assign more specific association type
 # TODO: include relation type
