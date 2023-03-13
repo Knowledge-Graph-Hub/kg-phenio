2565107230aff4a16ee65b6ed5cf58358d5a41b9
caufieldjh <j.harry.caufield@gmail.com>
2023-03-13 16:54:42 -0400

    Control for uberon/core properties

--- kg_phenio/transform_utils/phenio/phenio_edge_sources.py
+++ kg_phenio/transform_utils/phenio/phenio_edge_sources.py
@@ -127,7 +127,10 @@ aggregator_knowledge_source = "infores:phenio"
 
 subj_curie_prefix = (str(row["subject"]).split(":"))[0]
 if subj_curie_prefix == "OBO":  # See if there's another prefix
-    subj_curie_prefix = (str(row["subject"]).split("_"))[0][4:]
+    if (str(row["subject"])).startswith("OBO:uberon"):
+        subj_curie_prefix = "uberon"
+    else:
+        subj_curie_prefix = (str(row["subject"]).split("_"))[0][4:]
 obj_curie_prefix = (str(row["object"]).split(":"))[0]
 relation_prefix = (str(row["relation"]).split(":"))[0]
 
