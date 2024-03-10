97d6719f0500c2fb77ec2df47306905ca00c0dbb
caufieldjh <j.harry.caufield@gmail.com>
2024-03-10 18:44:33 -0400

    Update import name in upheno_mapping Tx

--- kg_phenio/transform_utils/upheno/upheno_mapping_all.py
+++ kg_phenio/transform_utils/upheno/upheno_mapping_all.py
@@ -1,7 +1,7 @@
 """Koza ingest for Upheno phenotype maps."""
 import uuid
 
-from biolink.model import Association, PhenotypicFeature
+from biolink_model.datamodel.pydanticmodel_v2 import Association, PhenotypicFeature
 from koza.cli_runner import get_koza_app
 
 source_name = "upheno_mapping_all"
